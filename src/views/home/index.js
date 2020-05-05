8527ae2e3066e77f50ed50d6ab88b65fef1a6f4a
aibaoxiaodon <sxczhxd@163.com>
2020-05-05 09:33:19 +0800

    对接微信

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 
-export default class Login extends Component {
+export default class Home extends Component {
+    onWeixin() {
+        // document.location.href = 'weixin://dl/scan'
+        window.location.href = 'http://weixin.qq.com/mp/qrcode?_biz=MjM5NTA3NzUwMQ=='
+    }
     render() {
         return (
-            <div>首页</div>
+            <div>
+                <button onClick={() => this.onWeixin()}>点击打开微信</button>
+            </div>
         )
     }
 }
