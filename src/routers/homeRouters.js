102504374c61cc7dd0828023abf1de0f8a1d90ba
hxd <xiaodong.han@fenbeitong.com>
2020-12-31 12:51:36 +0800

    支付宝调研

--- src/routers/homeRouters.js
+++ src/routers/homeRouters.js
@@ -1,14 +1,14 @@
 import asyncComponent from '../components/asyncComponent'
 
-// const Home = asyncComponent(() => import('../views/home'))
-const HxdReact = asyncComponent(() => import('../views/hxdReact'))
+const Home = asyncComponent(() => import('../views/home'))
+// const HxdReact = asyncComponent(() => import('../views/hxdReact'))
 
 const routerList = [
     {
         path: '/home',
         name: 'Home',
         exact: true,
-        component: HxdReact
+        component: Home
     }
 ]
 export default routerList
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react'
 import { Button } from 'antd-mobile'
 
 export default class Home extends Component {
-    onWeixin() {
-        document.location.href = 'weixin://'
-        // window.location.href = 'https://wdfp.5ifapiao.com/my-invoice/html/public/footer.html#invoice_list_reimbursement_bx'
-    }
-    render() {
-        return (
-            <div>
-                <button onClick={() => this.onWeixin()}>点击打开微信</button>
-                <Button type="primary" onClick={() => this.onWeixin()}>点击打开微信</Button>
-            </div>
-        )
-    }
+  onWeixin() {
+    document.location.href = 'weixin://'
+    // window.location.href = 'https://wdfp.5ifapiao.com/my-invoice/html/public/footer.html#invoice_list_reimbursement_bx'
+  }
+
+  onAlipay() {
+    // document.location.href = 'alipays://platformapi/startapp?saId=10000007&clientVersion=3.7.0.0718&qrcode=https://render.alipay.com/p/h5/shebei/index.html?ct=ZJXL&__webview_options__=transparentTitle%3Dalways'
+
+    document.location.href = 'alipays://'
+  }
+
+  render() {
+    return (
+      <div>
+        <Button type='primary' onClick={() => this.onWeixin()}>
+          点击打开微信
+        </Button>
+        <br />
+        <Button type='primary' onClick={() => this.onAlipay()}>
+          点击打开支付宝
+        </Button>
+      </div>
+    )
+  }
 }
